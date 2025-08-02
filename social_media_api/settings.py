377a5c1610481ea5e60ea09f1ca272921797afe5
Sasha <kluzodota@gmail.com>
2025-08-02 13:05:24 +0300

    add DRF to settings

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'social_django',
+    'socialmedia',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +118,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
